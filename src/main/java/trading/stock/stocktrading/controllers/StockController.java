eb492f6aca0d06d104590babff679c3b066de9d1
khatl.fe <luutkha@gmail.com>
2024-05-07 21:36:54 +0700

    Update StockController.java

--- src/main/java/trading/stock/stocktrading/controllers/StockController.java
+++ src/main/java/trading/stock/stocktrading/controllers/StockController.java
@@ -1,7 +1,6 @@
 package trading.stock.stocktrading.controllers;
 
 import lombok.extern.log4j.Log4j2;
-import org.mockito.internal.matchers.Null;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +11,7 @@ import trading.stock.stocktrading.dtos.StockDetailResponseDTO;
 import trading.stock.stocktrading.services.StockService;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/stock")
@@ -26,17 +26,15 @@ public class StockController {
 
     @GetMapping()
     public ResponseEntity<StockDetailResponseDTO> getStockDetailByCodeInCurrentTime(@RequestParam String symbol, @RequestParam(required = false) Long from, @RequestParam(required = false) Long to) throws IOException {
-        if (Null.NULL.matches(from) || Null.NULL.matches(to)) {
-            String stockDetailByCode = stockService.getStockDetailByCodeInCurrentTime(symbol).block();
-            StockDetailDTO stockDetail = StockDetailDTO.fromJson(stockDetailByCode);
-            StockDetailResponseDTO response = StockDetailResponseDTO.fromStockDetailDTO(stockDetail);
-            return ResponseEntity.ok(response);
+        String stockDetailByCode;
+        if (Objects.deepEquals(null, from) || Objects.deepEquals(null, to)) {
+            stockDetailByCode = stockService.getStockDetailByCodeInCurrentTime(symbol).block();
         } else {
-            String stockDetailByCode = stockService.getStockDetailByCodeAndTime(symbol, from, to).block();
-            StockDetailDTO stockDetail = StockDetailDTO.fromJson(stockDetailByCode);
-            StockDetailResponseDTO response = StockDetailResponseDTO.fromStockDetailDTO(stockDetail);
-            return ResponseEntity.ok(response);
+            stockDetailByCode = stockService.getStockDetailByCodeAndTime(symbol, from, to).block();
         }
+        StockDetailDTO stockDetail = StockDetailDTO.fromJson(stockDetailByCode);
+        StockDetailResponseDTO response = StockDetailResponseDTO.fromStockDetailDTO(stockDetail);
+        return ResponseEntity.ok(response);
 
     }
 
